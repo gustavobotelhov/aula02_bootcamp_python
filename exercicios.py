4a4419a4b4a4bc3b8c35c8679d8292b93db56659
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-08-19 22:33:40 -0300

    adicionando exercicio 7

--- exercicios.py
+++ exercicios.py
@@ -34,12 +34,17 @@ import math
 
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
 
-numero_01 = float(input("Digite um numero: "))
-numero_02 = float(input("Digite outro numero: "))
-print(numero_01 + numero_02)
-
+# numero_01 = float(input("Digite um numero: "))
+# numero_02 = float(input("Digite outro numero: "))
+# print(numero_01 + numero_02)
 
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+
+numero_01 = float(input("Digite um numero: "))
+numero_02 = float(input("Digite outro numero: "))
+media = (numero_01+numero_02) / 2
+print(media)
+
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
