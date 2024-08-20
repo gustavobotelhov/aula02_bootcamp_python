9d18962a2ae67e0e7bf50f35fe45c45fb3b50e4e
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-08-20 17:58:00 -0300

    adicionando exercicio 19

--- exercicios.py
+++ exercicios.py
@@ -100,18 +100,36 @@ import math
 
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
-string_1 = input("Digite um texto: ")
-string_2 = input("Digite outro texto: ")
+# string_1 = input("Digite um texto: ")
+# string_2 = input("Digite outro texto: ")
 
-print(string_1+" "+string_2)
-
+# print(string_1+" "+string_2)
 
 # #### Booleanos (`bool`)
 
 # 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
+# expressao_1 = input("Digite uma expressao booleana: ")
+# expressao_2 = input("Digite outra expressao booleana: ")
+
 # 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+
+# valor_1 = input("Digite uma valor: ")
+# valor_2 = input("Digite outro valor: ")
+
 # 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+
+valor_1 = input("Digite uma valor: ")
+valor_2 = input("Digite outro valor: ")
+
+resultado = valor_1 == valor_2
+
+if valor_1 == valor_2:
+    print("Os dois números são iguais.")
+else:
+    print("Os dois números são diferentes.")
+
 # 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
 
 # #### try-except e if
