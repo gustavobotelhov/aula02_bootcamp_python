c4255b73e067c8cc2a6b627a07ae6ede4e10c737
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-08-19 21:41:04 -0300

    adicionando exercicios 2

--- exercicios.py
+++ exercicios.py
@@ -1,10 +1,13 @@
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-numero_01 = int(input("Digite um numero: "))
-numero_02 = int(input("Digite outro numero: "))
-print(numero_01+numero_02)
-
+# numero_01 = int(input("Digite um numero: "))
+# numero_02 = int(input("Digite outro numero: "))
+# print(numero_01+numero_02)
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+numero_01 = int(input("Digite um numero: "))
+resultado = numero_01 % 5
+print(resultado)
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
