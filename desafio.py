1934f424ffb0e9ddc081fe9b63b06f58deacd49d
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-08-20 22:23:21 -0300

    adicionando exercicio 5 e finalizando o desafio.

--- desafio.py
+++ desafio.py
@@ -4,17 +4,17 @@
 
 # e se o usuario digitar um número? explorar possibilidades de erro.
 
-# nome_usuario = input("Digite o seu nome: ")
+nome_usuario = input("Digite o seu nome: ")
 
-# if nome_usuario.isdigit():
-#     print("Você digitou o seu nome errado.")
-#     exit()
-# elif len(nome_usuario) == 0:
-#     print("Você não digitou nada.")
-#     exit()
-# elif nome_usuario.isspace():
-#     print("Você digitou só espaço")
-#     exit()
+if nome_usuario.isdigit():
+    print("Você digitou o seu nome errado.")
+    exit()
+elif len(nome_usuario) == 0:
+    print("Você não digitou nada.")
+    exit()
+elif nome_usuario.isspace():
+    print("Você digitou só espaço")
+    exit()
 
 # 2) Solicita ao usuário que digite o valor do seu salário
 # Converte a entrada para um número de ponto flutuante
@@ -31,12 +31,19 @@ except ValueError:
 # 3) Solicita ao usuário que digite o valor do bônus recebido
 # Converte a entrada para um número de ponto flutuante
 
-# bonus_usuario = float(input("Digite o bonus recebido: "))
+bonus_usuario = float(input("Digite o seu salario: "))
+
+try:
+    if bonus_usuario < 0:
+        print("Por favor, digite um valor positivo para o salário.")
+except ValueError:
+    print("Entrada inválida para o salário. Por favor, digite um número.")
+    exit()
 
 # 4) Calcule o valor do bônus final
 
 # 5) Imprime a mensagem personalizada incluindo o nome do usuário, salário e bônus
 
-# print(f"O usuario {nome_usuario} recebeu o bonus de: {bonus_usuario}")
+print(f"O usuario {nome_usuario} com salario de {salario_usuario} e com o bonus de: {bonus_usuario}")
 
 # Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
